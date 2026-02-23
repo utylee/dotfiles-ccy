01ebffd242054f1aaa5b1d60d7b6304778162eeb
utylee <utylee@gmail.com>
2026-02-24 06:34:48 +0900

    add: stuffs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,11 +17,16 @@ end
 #echo -ne '\eP\e]12;#6d207a\a'	# HC2
 #set -g fish_key_bindings fish_vi_key_binding
 
+set -U fish_history_max 200000
 
 set -Ux fish_term24bit 1
 
 set -gx EDITOR /usr/bin/vi
 set -gx GHQ_ROOT /home/utylee/.ghq
+
+# for llama-server of wsl2 ssh
+set -gx LLS_MODELS_DIR /home/utylee/temp/llm_models
+set -gx LLS_LLAMA_BIN  /home/utylee/temp/llama.cpp/build-vk/bin/llama-server
 
 set -x LANG ko_KR.UTF-8
 set -x LANGUAGE ko_KR:ko
@@ -30,14 +35,17 @@ set -x LC_ALL ko_KR.UTF-8
 # PATH
 #set CLANGHOME /usr/local/clang-14-dev
 #set CLANGHOME /usr/local/clang+llvm-12.0.1-armv7a-linux-gnueabihf
-set CLANGHOME /usr/local/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04
+#set CLANGHOME /usr/local/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04
 # set -l NODEHOME /usr/local/node-v22.18.0-linux-armv7l
-set -l NODEHOME /usr/local/node-v22.18.0-linux-x64
-set -x PATH $CLANGHOME/bin $PATH
+# set -l NODEHOME /usr/local/node-v22.18.0-linux-x64
+set -l NODEHOME /usr/local/node-v22.22.0-linux-x64
+# set -x PATH $CLANGHOME/bin $PATH
 set -x PATH $HOME/.go/bin /usr/local/go1.17.3/bin $NODEHOME/bin $PATH
-set -gx CC $CLANGHOME/bin/clang
-set -gx CXX $CLANGHOME/bin/clang++
-set -gx LD_LIBRARY_PATH $CLANGHOME/lib $LD_LIBRARY_PATH 
+
+fish_add_path /home/utylee/temp/bin  /home/utylee/temp/llama.cpp/build-vk/bin
+#set -gx CC $CLANGHOME/bin/clang
+#set -gx CXX $CLANGHOME/bin/clang++
+#set -gx LD_LIBRARY_PATH $CLANGHOME/lib $LD_LIBRARY_PATH 
 
 # FZF
 # fzf을 직접입력해 파일명 탐색 명내용 
